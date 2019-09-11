0b44a5083e55e295e0570e9e29787a0dea2f85a4
bszeti <bszeti@redhat.com>
2019-09-10 23:42:31 -0400

    Add image to template

---
+++ battlefield-quay.sh
@@ -0,0 +1,7 @@
+oc delete all -l template=battlefield-test
+oc process -f battlefield-template.yaml -p IMAGE='quay.io/bszeti/fuse-player:latest' -p PLAYER_NAME=p0 -p PLAYER_URLS=p1,p2 -oyaml | oc create -f -
+oc process -f battlefield-template.yaml -p IMAGE='quay.io/bszeti/fuse-player:latest' -p PLAYER_NAME=p1 -p PLAYER_URLS=p0,p2 -oyaml | oc create -f -
+oc process -f battlefield-template.yaml -p IMAGE='quay.io/bszeti/fuse-player:latest' -p PLAYER_NAME=p2 -p PLAYER_URLS=p0,p1 -oyaml | oc create -f -
+
+# oc rsh p1
+# curl http://p0/hit/me
